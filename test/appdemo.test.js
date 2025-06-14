4ac30c77546fb75b2ebe992c6e9ec0e448fd2f4c
Luki R <thekucays@gmail.com>
2025-06-14 12:27:54 +0700

    feat: add more scenarios

--- test/appdemo.test.js
+++ test/appdemo.test.js
@@ -2,6 +2,22 @@ import { expect } from 'chai';
 
 describe('Appium DEMO App', () => {
 
+  it('Klik elemen', async () => {
+    // menu: home page
+    const elemenKlik = await $(`//android.widget.TextView[@content-desc="Accessibility"]`);
+    await elemenKlik.click();
+  });
+  
+  it('Input ke elemen', async () => {
+    // menu: App > Alert Dialogs > Text Entry dialog
+    const appMenu = await $(`//android.widget.TextView[@content-desc="App"]`)
+    const alertdialogMenu = await $(`//android.widget.TextView[@content-desc="Alert Dialogs"]`)
+    const textentryMenu = await $(`//android.widget.Button[@content-desc="Text Entry dialog"]`)
+    await appMenu.click();
+    await alertdialogMenu.click();
+    await textentryMenu.click();
+  });
+  
   it('Memastikan App bisa terbuka dan elemen tersedia', async () => {
     const accessibilityMenu = await $(`//android.widget.TextView[@content-desc="Accessibility"]`);
 
@@ -12,7 +28,7 @@ describe('Appium DEMO App', () => {
   });
 
   it('Scroll elemen hard-coded', async () => {
-    // menu: Views/Gallery/1. Photos
+    // menu: Views > Gallery > 1. Photos
     const viewsMenu = await $(`//android.widget.TextView[@content-desc="Views"]`);
     const galleryMenu = await $(`//android.widget.TextView[@content-desc="Gallery"]`);
     const photosMenu = await $(`//android.widget.TextView[@content-desc="1. Photos"]`);
@@ -48,13 +64,50 @@ describe('Appium DEMO App', () => {
         { type: 'pointerUp', button: 0 }
       ]
     }]);
-
     // Always release actions after use
     await browser.releaseActions();
+
+    // case: scroll scrollview sampai ketemu element
+    /*const scrollView = await $(`android=new UiSelector().resourceId("abc")`);
+    const targetElement = await $(`//android.widget.Gallery[@resource-id="io.appium.android.apis:id/gallery"]`);
+
+    let maxScrolls = 5;
+    let isVisible = await targetElement.isDisplayed().catch(() => false);
+
+    while (!isVisible && maxScrolls > 0) {
+      const location = await scrollView.getLocation();
+      const size = await scrollView.getSize();
+
+      const centerX = location.x + size.width / 2;
+      const startY = location.y + size.height * 0.8;
+      const endY = location.y + size.height * 0.2;
+
+      // Swipe up inside the scrollView
+      await browser.performActions([{
+        type: 'pointer',
+        id: 'finger1',
+        parameters: { pointerType: 'touch' },
+        actions: [
+          { type: 'pointerMove', duration: 0, x: centerX, y: startY },
+          { type: 'pointerDown', button: 0 },
+          { type: 'pause', duration: 200 },
+          { type: 'pointerMove', duration: 300, x: centerX, y: endY },
+          { type: 'pointerUp', button: 0 }
+        ]
+      }]);
+      await browser.releaseActions();
+
+      // Wait briefly before checking again
+      await browser.pause(1000);
+
+      isVisible = await targetElement.isDisplayed().catch(() => false);
+      maxScrolls--;
+    }
+
+    expect(isVisible).to.be.true;
+    */
+
   });
 
-  it('Input ke elemen', async () => {
-    // menu: ddds
-  });
 
 });
